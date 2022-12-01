58cbb28dc73ac887febfbe39f8653edeca79564a
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-01 04:15:25 -0800

    Improve comment clarity of Settings.java

--- src/Settings.java
+++ src/Settings.java
@@ -2,14 +2,14 @@ public class Settings {
     public static String user = ""; // for AD login
     public static String pass = ""; // for AD login
 
-    public static String inHour = ""; // [1-12]
-    public static String inMinute = ""; // 00 15 30 45
-    public static String inAP = ""; // A or P (AM or PM)
+    public static String inHour = ""; // Any number between 1 to 12, inclusive.
+    public static String inMinute = ""; // 00 or 15 or 30 or 45
+    public static String inAP = ""; // A or P (represents AM or PM)
     public static String inTime = inHour + ":" + inMinute + " " + inAP + "M";
 
-    public static String outHour = ""; // [1-12]
-    public static String outMinute = ""; // 00 15 30 45
-    public static String outAP = ""; // A or P (AM or PM)
+    public static String outHour = ""; // Any number between 1 to 12, inclusive.
+    public static String outMinute = ""; // 00 or 15 or 30 or 45
+    public static String outAP = ""; // A or P (pepresents AM or PM)
     public static String outTime = outHour + ":" + outMinute + " " + outAP + "M";
 
     public static String date = ""; // MM/DD format
@@ -18,5 +18,5 @@ public class Settings {
     public static String payCode = ""; // Hours Worked OR On Call/Call Back Worked
 
     public static int waitFor2FA = 10; // in seconds
-    public static int waitForSelect = 250; // in milliseconds
+    public static int waitForSelect = 500; // in milliseconds
 }
